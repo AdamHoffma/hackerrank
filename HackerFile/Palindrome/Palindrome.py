79e0cfbe2f8d610c0db9ba11849231c6643a3bc4
Adam Hoffman <wintros@yahoo.com>
2020-03-31 15:25:36 -0600

    how to call...

--- HackerFile/Palindrome/Palindrome.py
+++ HackerFile/Palindrome/Palindrome.py
@@ -1,7 +1,5 @@
 class Solution:
-    def countSubstrings(self, string):
-
-        counter = len(string)
+    def countSubstrings(self, string):        
         lp = 0
         rp = lp + 1
         tempstr = list(string[lp] + string[rp])
@@ -12,7 +10,7 @@ class Solution:
         print(tempstr)
         while lp < len(string) -1:
             if string[rp] == string[lp]:
-                substring(tempstr)
+                self.substring(tempstr, string)
                 rp + 1
             elif string[rp] != string[lp]:
                 rp + 1
@@ -22,7 +20,25 @@ class Solution:
                 rp = lp + 1
                 tempstr = list(string[lp] + string[rp])
     
-    def substring(self, counter, tempstr):
+    def substring(self, tempstr, string):
+        counter = len(string)
+        if len(tempstr) <= 3:
+            return counter + 1
+        else:
+            lsubp = 1
+            rsubp = len(tempstr) -2
+
+            while lsubp < rsubp:
+                if tempstr[lsubp] == tempstr[rsubp] and lsubp + 1 >= rsubp:
+                    return counter + 1
+                elif tempstr[lsubp] == tempstr[rsubp]:
+                    lsubp + 1
+                    rsubp -1
+                else:
+                    break
+        return counter
+
+print(Solution.countSubstrings("abd"))
 
 
 
