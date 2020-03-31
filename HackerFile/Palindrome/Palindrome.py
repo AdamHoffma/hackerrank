2bb4994aeac0334b65506874bf6c15609b0c3093
Adam Hoffman <wintros@yahoo.com>
2020-03-31 15:11:13 -0600

    substring function

--- HackerFile/Palindrome/Palindrome.py
+++ HackerFile/Palindrome/Palindrome.py
@@ -1,22 +1,32 @@
-   
-def countSubstrings(string):
+class Solution:
+    def countSubstrings(self, string):
+
+        counter = len(string)
+        lp = 0
+        rp = lp + 1
+        tempstr = list(string[lp] + string[rp])
+        print (len(string)-1) 
+        #print(rp + 1)
+        print(string[lp])
+        print(string[rp])
+        print(tempstr)
+        while lp < len(string) -1:
+            if string[rp] == string[lp]:
+                substring(tempstr)
+                rp + 1
+            elif string[rp] != string[lp]:
+                rp + 1
+                tempstr.append(string[rp])
+            else:
+                lp + 1
+                rp = lp + 1
+                tempstr = list(string[lp] + string[rp])
     
-    counter = len(string)
-    lp = 0
-    rp = lp + 1
-    tempstr = list(string[lp] + string[rp])
-    print (len(string)-1) 
-    #print(rp + 1)
-    print(string[lp])
-    print(string[rp])
-    print(tempstr)
-    while lp < len(string) -1:
-        if string[rp] == s[lp]:
-            substring(tempstr)
-            rp + 1
+    def substring(self, counter, tempstr):
 
 
-countSubstrings("abcdes")
+
+
 
 
 
