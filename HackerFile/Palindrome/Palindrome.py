8f26e0cc53f7859e1d30e42ed4cba457971d209d
Adam Hoffman <wintros@yahoo.com>
2020-03-31 15:04:58 -0600

    fixed the pointers

--- HackerFile/Palindrome/Palindrome.py
+++ HackerFile/Palindrome/Palindrome.py
@@ -2,13 +2,18 @@
 def countSubstrings(string):
     
     counter = len(string)
-    lp = string[:1]
-    rp = string[1:2]
-    tempstr = lp + rp 
-    print(lp)
-    print('rp', rp)
+    lp = 0
+    rp = lp + 1
+    tempstr = list(string[lp] + string[rp])
+    print (len(string)-1) 
+    #print(rp + 1)
+    print(string[lp])
+    print(string[rp])
     print(tempstr)
-        
+    while lp < len(string) -1:
+        if string[rp] == s[lp]:
+            substring(tempstr)
+            rp + 1
 
 
 countSubstrings("abcdes")
