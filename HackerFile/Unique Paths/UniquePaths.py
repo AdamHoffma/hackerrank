1112400b8bfc5ed467f71e8b05b953b28b516679
Adam Hoffman <wintros@yahoo.com>
2020-04-01 17:21:34 -0600

    what is cur[j]?

--- HackerFile/Unique Paths/UniquePaths.py
+++ HackerFile/Unique Paths/UniquePaths.py
@@ -1,17 +1,30 @@
 import timeit
 
-code_to_test = """
-def uniquePaths(n, m):    
+# code_to_test = """
+# def uniquePaths(n, m):    
     
-    if n == 1 or m == 1:
-        return 1        
+#     if n == 1 or m == 1:
+#         return 1        
     
-    return uniquePaths(n - 1, m) + uniquePaths(n, m - 1)
-print(uniquePaths(13, 13))
-    """
+#     return uniquePaths(n - 1, m) + uniquePaths(n, m - 1)
+# print(uniquePaths(15, 15))
+#     """
 
-elapsed_time = timeit.timeit(code_to_test, number=1)/1
-print(elapsed_time)
+# elapsed_time = timeit.timeit(code_to_test, number=1)/1
+# print(elapsed_time)
+
+
+def paths(m, n):
+    if not m or not n:
+        return 0
+    cur = [1] * n
+    for i in range(1, m):
+        for j in range(1, n):
+            cur[j] += cur[j-1]
+            print(cur[j])
+    return cur[-1]
+
+print(paths(7, 3))
 
 
 
