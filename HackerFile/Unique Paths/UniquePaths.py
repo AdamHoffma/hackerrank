70ad31552d6853e42f3635972ac6204ed953a97d
Adam Hoffman <wintros@yahoo.com>
2020-04-01 14:43:24 -0600

    interesting timer function, makes the function a string, passes the string to timeit which runs the code

--- HackerFile/Unique Paths/UniquePaths.py
+++ HackerFile/Unique Paths/UniquePaths.py
@@ -1,7 +1,15 @@
-def uniquePaths(n, m):
+import timeit
+
+code_to_test = """
+def uniquePaths(n, m):    
+    
     if n == 1 or m == 1:
-        return 1
+        return 1        
     
     return uniquePaths(n - 1, m) + uniquePaths(n, m - 1)
-
 print(uniquePaths(7, 7))
+    """
+
+elapsed_time = timeit.timeit(code_to_test, number=1)/1
+print(elapsed_time)
+#print(uniquePaths(7, 7))
