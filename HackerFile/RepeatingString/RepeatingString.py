e99ac7d6fe3dcb1e7f1efcbb511ff3352c8031c7
Adam Hoffman <wintros@yahoo.com>
2020-04-07 10:56:11 -0600

    loops

---
+++ HackerFile/RepeatingString/RepeatingString.py
@@ -0,0 +1,16 @@
+def repeatingString(s, n):
+    pointer = 0
+    for i in s:
+        pointer += 1
+        print(pointer)
+        print(i)
+        if pointer == len(s):
+            pointer = 0
+            for x in s:
+                pointer += 1
+                print(pointer)
+                print(x)
+
+    return pointer
+
+print(repeatingString("Thisisthesavage"))
---
+++ HackerFile/RepeatingString/notes.md
@@ -0,0 +1,3 @@
+# split the string into an array of characters
+
+3  10 / 3 = 3.33
